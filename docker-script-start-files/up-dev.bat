6140f870bc00b81187f7067b7ab64b54fbc532d3
PauloPortfolio <pvba02@gmail.com>
2020-09-19 15:57:02 -0600

    Starting the project

---
+++ docker-script-start-files/up-dev.bat
@@ -0,0 +1,26 @@
+echo on
+REM MAVEN-CLEAN-PACKAGE WITHOUT TESTS
+cd ..
+call mvn clean package -DskipTests
+cd docker-script-start-files
+
+docker-compose -f ../dev-compose.yml down --remove-orphans
+docker-compose -f ../test-compose.yml down --remove-orphans
+
+docker container prune --force
+docker container rm $(docker container ls -q)
+
+docker system prune --volumes --force
+docker volume rm $(docker volume ls -q)
+
+docker image prune --force
+docker image rm pauloportfolio/web-api
+
+docker system df
+docker image ls
+
+rem START THE COMPOSE CONTAINERS
+docker-compose -f ../dev-compose.yml up --build --force-recreate
+
+pause
+
---
+++ src/main/java/com/spring_webflux_r2dbc_relationship/AppDriver.java
@@ -0,0 +1,13 @@
+package com.spring_webflux_r2dbc_relationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDriver {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppDriver.class,args);
+    }
+
+}
